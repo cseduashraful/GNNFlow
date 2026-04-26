1e519fc28e50aa2a7cffb604d44e40ed1a082d8d
Md Ashraful Islam <mdashrafulis@umass.edu>
2026-04-26 13:29:59 -0400

    Add downloader fallback for dataset script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 # from https://github.com/amazon-research/tgl/blob/main/down.sh
 
-mkdir -p ../data/MOOC/
-aria2c -x 16 -d ../data/MOOC https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MOOC/edges.csv
-mkdir -p ../data/REDDIT/
-aria2c -x 16 -d ../data/REDDIT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/REDDIT/edge_features.pt
-aria2c -x 16 -d ../data/REDDIT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/REDDIT/edges.csv
-aria2c -x 16 -d ../data/REDDIT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/REDDIT/labels.csv
-mkdir -p ../data/WIKI
-aria2c -x 16 -d ../data/WIKI https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/WIKI/edge_features.pt
-aria2c -x 16 -d ../data/WIKI https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/WIKI/edges.csv
-aria2c -x 16 -d ../data/WIKI https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/WIKI/labels.csv
-mkdir -p ../data/LASTFM/
-aria2c -x 16 -d ../data/LASTFM https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/LASTFM/edges.csv
-mkdir -p ../data/GDELT/
-aria2c -x 16 -d ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/node_features.pt
-aria2c -x 16 -d ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/labels.csv
-aria2c -x 16 -d ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/edges.csv
-aria2c -x 16 -d ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/edge_features.pt
-mkdir -p ../data/MAG/
-aria2c -x 16 -d ../data/MAG https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MAG/labels.csv
-aria2c -x 16 -d ../data/MAG https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MAG/edges.csv
-aria2c -x 16 -d ../data/MAG https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MAG/node_features.pt
+set -euo pipefail
 
+download_file() {
+  local output_dir="$1"
+  local url="$2"
+  local filename
+
+  mkdir -p "$output_dir"
+  filename="$(basename "$url")"
+
+  if command -v aria2c >/dev/null 2>&1; then
+    aria2c -x 16 -d "$output_dir" "$url"
+  elif command -v curl >/dev/null 2>&1; then
+    curl -L --fail --output "$output_dir/$filename" "$url"
+  elif command -v wget >/dev/null 2>&1; then
+    wget -O "$output_dir/$filename" "$url"
+  else
+    echo "Error: no supported downloader found. Install aria2, curl, or wget." >&2
+    exit 1
+  fi
+}
+
+download_file ../data/MOOC https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MOOC/edges.csv
+
+download_file ../data/REDDIT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/REDDIT/edge_features.pt
+download_file ../data/REDDIT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/REDDIT/edges.csv
+download_file ../data/REDDIT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/REDDIT/labels.csv
+
+download_file ../data/WIKI https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/WIKI/edge_features.pt
+download_file ../data/WIKI https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/WIKI/edges.csv
+download_file ../data/WIKI https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/WIKI/labels.csv
+
+download_file ../data/LASTFM https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/LASTFM/edges.csv
+
+download_file ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/node_features.pt
+download_file ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/labels.csv
+download_file ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/edges.csv
+download_file ../data/GDELT https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/GDELT/edge_features.pt
+
+download_file ../data/MAG https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MAG/labels.csv
+download_file ../data/MAG https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MAG/edges.csv
+download_file ../data/MAG https://s3.us-west-2.amazonaws.com/dgl-data/dataset/tgl/MAG/node_features.pt
